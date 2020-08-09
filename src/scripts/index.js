c8170373ad9d69f6360d8a139181c3cd8cf1a232
Artem <temaspsxxx@gmail.com>
2020-08-09 19:08:22 +0300

    Adaptive done

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,9 @@
+'use strict';
 
+const navbar = document.querySelector('.navbar');
+const navbarButton = document.querySelector('.navbar__button');
+
+navbarButton.onclick = () => {
+  navbar.classList.toggle('navbar--open');
+  navbarButton.calssList.toggle('navbar__button--active');
+}
